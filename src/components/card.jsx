088a9ecdb85437d7acb6c62430c2e902ef5d7218
Isc Miguel Angel <isc.mangel.glzdelarosa@gmail.com>
2022-10-18 10:24:14 -0500

    Second commit - accessibility corrections

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -24,15 +24,15 @@ const Card = () => {
     return (
         <div className="card">
             <div className="card-header">
-                <p>ADVICE # <span>{ advice.id }</span></p>
+                <h1>ADVICE # <span>{ advice.id }</span></h1>
             </div>
             <div className="card-body">
                 <p><q>{ advice.advice }</q></p>
-                <img src={ divisor } />
+                <img src={ divisor } alt="Divider" />
             </div>
             <div className="card-footer">
                 <button type="button" onClick={ () => handleAdvice() } className={`${isLoading ? "load" : ""}`}>
-                    <img src={ icon_dice } />
+                    <img src={ icon_dice } alt="Icon button" /><p style={ { display: "none" } }>Click to generate</p>
                 </button> 
             </div>
         </div>
